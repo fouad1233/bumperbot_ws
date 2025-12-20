5aa201881e5fa60c400752d58ddf1628fc5dbbf1
fouad123 <fouadiadhami@gmail.com>
2025-12-20 14:08:25 +0300

    Add control by using diff_drive_controller

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 def generate_launch_description():
@@ -17,31 +17,15 @@ def generate_launch_description():
         'wheel_seperation', default_value='0.17',
         description='Seperation between the wheels'
     )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        'use_simple_controller', default_value='True',
+        description='Whether to use the simple controller or not'
+    )
     # Read the run-time value of the parameters
     use_python = LaunchConfiguration('use_python') 
     wheel_radius = LaunchConfiguration('wheel_radius')
     wheel_seperation = LaunchConfiguration('wheel_seperation')
-    
-    simple_controller_py = Node(
-        package="bumperbot_controller",
-        executable="simple_controller.py",
-        name="simple_controller",
-        parameters=[{
-            'wheel_radius': wheel_radius,
-            'wheel_seperation': wheel_seperation
-        }],
-        condition=IfCondition(use_python)
-    )
-    simple_controller_cpp = Node(
-        package="bumperbot_controller",
-        executable="simple_controller",
-        name="simple_controller",
-        parameters=[{
-            'wheel_radius': wheel_radius,
-            'wheel_seperation': wheel_seperation
-        }],
-        condition=UnlessCondition(use_python)
-    )
+    use_simple_controller = LaunchConfiguration('use_simple_controller')
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -52,21 +36,65 @@ def generate_launch_description():
             "/controller_manager"
         ]
     )
-    simple_controller = Node(
+    
+    wheel_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=[
-            "simple_velocity_controller",
+            "bumperbot_controller",
             "--controller-manager",
             "/controller_manager"
+        ],
+        condition=UnlessCondition(use_simple_controller)
+    )
+    ############################
+    simple_controller = GroupAction(
+        condition = IfCondition(use_simple_controller),
+        actions=  
+        [
+            # Spawner for the simple velocity controller
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "simple_velocity_controller",
+                    "--controller-manager",
+                    "/controller_manager"
+                ]
+            ),
+            # Simple Controller Node in Python
+            Node(
+                package="bumperbot_controller",
+                executable="simple_controller.py",
+                name="simple_controller",
+                parameters=[{
+                    'wheel_radius': wheel_radius,
+                    'wheel_seperation': wheel_seperation
+                }],
+                condition=IfCondition(use_python)
+            ),
+            # Simple Controller Node in C++
+            Node(
+                package="bumperbot_controller",
+                executable="simple_controller",
+                name="simple_controller",
+                parameters=[{
+                    'wheel_radius': wheel_radius,
+                    'wheel_seperation': wheel_seperation
+                }],
+                condition=UnlessCondition(use_python)
+            )
         ]
     )
+    
+
+
     return LaunchDescription([
         use_python_arg,
         wheel_radius_arg,
         wheel_seperation_arg,
+        use_simple_controller_arg,
         joint_state_broadcaster_spawner,
-        simple_controller,
-        simple_controller_py,
-        simple_controller_cpp
+        wheel_controller_spawner,
+        simple_controller
     ])
