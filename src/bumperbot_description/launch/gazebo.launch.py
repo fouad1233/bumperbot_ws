561e33d1c64ecee676fe60e659aca9f0b1eb9c88
fouad123 <fouadiadhami@gmail.com>
2026-01-17 12:24:16 +0300

    Fix gazebo.launch.py to make inverse kinematics works

--- src/bumperbot_description/launch/gazebo.launch.py
+++ src/bumperbot_description/launch/gazebo.launch.py
@@ -2,9 +2,8 @@ import os
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -12,68 +11,71 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
-
 def generate_launch_description():
-    bumperbot_description_dir = get_package_share_directory('bumperbot_description')
-    ros_distro = os.environ.get('ROS_DISTRO')
-    is_ignition = 'True' if ros_distro== "humble" else "False"
-    model_arg = DeclareLaunchArgument(
-        name='model',
-        default_value=os.path.join(
-            bumperbot_description_dir,
-            'urdf',
-            'bumperbot.urdf.xacro'
-        ),
-        description='Absolute path to robot urdf file'
+    bumperbot_description = get_package_share_directory("bumperbot_description")
+
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        bumperbot_description, "urdf", "bumperbot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
     )
-    # Here  we are using xacro to process the urdf file and load the model into robot_description parameter
-    robot_description = ParameterValue(
-        Command([
-            'xacro ',
-            LaunchConfiguration('model'),
-            ' is_ignition:=', is_ignition
-            ]),
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(bumperbot_description).parent.resolve())
+            ]
+        )
+    
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+    
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
         value_type=str
     )
+
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{
-            "robot_description": robot_description
-        }],
-    )
-    gazebo_resource_path = SetEnvironmentVariable(
-        name='GZ_SIM_RESOURCE_PATH',
-        value=[
-            str(Path(bumperbot_description_dir).parent.resolve())
-            ]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch')
-            ,'/gz_sim.launch.py'
-            ]),
-        launch_arguments=[
-                ('gz_args', [" -v 4", " -r", " empty.sdf"]
-                
-                ) # -v 4 for verbose console output, -r to reset simulation and start immediately
-            ]
-        )
-    # Spawn means to insert the robot model into the simulation
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
+                    )
+                ]
+             )
+
     gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen', # This make us seeing the simulation
-        arguments=[
-            '-topic', 'robot_description',
-            '-name', 'bumperbot'
-        ],
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "bumperbot"],
     )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ]
+    )
+
     return LaunchDescription([
         model_arg,
-        robot_state_publisher_node,
         gazebo_resource_path,
+        robot_state_publisher_node,
         gazebo,
-        gz_spawn_entity
+        gz_spawn_entity,
+        gz_ros2_bridge,
     ])
